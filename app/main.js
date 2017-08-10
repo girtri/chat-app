43b3c76b342e5c08834e5b1b53ca7e532a6fc8fe
gerry <girtri@libero.it>
2017-08-10 17:04:23 +0200

    Capitolo 3, pagina 121

--- app/main.js
+++ app/main.js
@@ -3,9 +3,17 @@ const { app, BrowserWindow } = require( "electron" ),
 	path = require( "path" ),
 	url = require( "url" );
 
+const { default: installExtension, REACT_DEVELOPER_TOOLS } = require( "electron-devtools-installer" );
+
+// Keep a global reference of the window object, if you don"t, the window will be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 
 function createWindow() {
+	// React Developer Tools
+	installExtension(REACT_DEVELOPER_TOOLS)
+    	.then((name) => console.log(`Added Extension: ${name}`))
+    	.catch((err) => console.log("An error occurred: ", err));
+
 	mainWindow = new BrowserWindow({
 		width: 1000, height: 600
 	});
